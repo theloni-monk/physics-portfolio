a4bd5930c35bd6a67543bad445ac0e7d96c4110f
Theo <13920881+theloni-monk@users.noreply.github.com>
2020-09-18 22:33:22 -0700

    built mvp

---
+++ docs/precache-manifest.af5749cae23e20b47afe81a5c5b21cab.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "172152bd351e6f923e236676f53316b4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c334d6b9ad7681d3eb15",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2d5a41393aedfd39d637",
+    "url": "/static/js/2.cdcd3363.chunk.js"
+  },
+  {
+    "revision": "69f8dcef8d7d4d3ab122841edcf3450b",
+    "url": "/static/js/2.cdcd3363.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c334d6b9ad7681d3eb15",
+    "url": "/static/js/main.0711b023.chunk.js"
+  },
+  {
+    "revision": "db694aa70f6bdf0b769a",
+    "url": "/static/js/runtime-main.e1b6f5e9.js"
+  },
+  {
+    "revision": "7fb5be4a7cc08be4e928edc1dc03991e",
+    "url": "/static/media/rand.7fb5be4a.jpg"
+  }
+]);
