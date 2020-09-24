86c3e18c0952d695c6efcd3a14d8370a35c009eb
Theo <13920881+theloni-monk@users.noreply.github.com>
2020-09-24 15:59:54 -0700

    refactored vector code to pure functions

--- src/simulation/utils/vect.ts
+++ src/simulation/utils/vect.ts
@@ -1,47 +1,35 @@
-//WRITEME: vector code
 /**@class Immutable Vector class, all methods return either properties or new vectors */
-export default class Vector2{
-    readonly x:number;
-    readonly y:number;
+export default class Vector2 {
+    readonly x: number;
+    readonly y: number;
 
-    constructor(x:number, y:number){
+    constructor(x: number, y: number) {
         this.x = x;
         this.y = y;
     }
 
-    static fromPolar(r:number, theta:number){
-        return new Vector2(r*Math.cos(theta), r*Math.sin(theta))
-    }
+    static fromPolar = (r: number, theta: number): Vector2 => new Vector2(r * Math.cos(theta), r * Math.sin(theta))
 
-    length():number{
-        return Math.sqrt(this.x*this.x + (this.y*this.y))
-    }
 
-    radians():number {
-        return Math.atan2(this.y, this.x)
-    }
+    length = (): number => Math.sqrt(this.x * this.x + (this.y * this.y))
 
-    congugate():Vector2{
-        return new Vector2(this.x, -this.y)
-    }
+    radians = (): number => Math.atan2(this.y, this.x)
 
-    inv():Vector2{
-        return new Vector2(-this.x, -this.y)
-    }
+    congugate = (): Vector2 => new Vector2(this.x, -this.y)
 
-    add(other:Vector2):Vector2{
-        return new Vector2(this.x+other.x, this.y+other.y);
-    }
 
-    sub(other:Vector2):Vector2{
-        return new Vector2(this.x-other.x, this.y-other.y);
-    }
+    inv = (): Vector2 => new Vector2(-this.x, -this.y)
 
-    multScalar(s:number):Vector2{
-        return new Vector2(this.x*s, this.y*s)
-    }
 
-    dot(other:Vector2):number{
-        return this.x*other.x+(this.y*other.y)
-    }
+    add = (other: Vector2): Vector2 => new Vector2(this.x + other.x, this.y + other.y);
+
+
+    sub = (other: Vector2): Vector2 => new Vector2(this.x - other.x, this.y - other.y);
+
+
+    multScalar = (s: number): Vector2 => new Vector2(this.x * s, this.y * s)
+
+
+    dot = (other: Vector2): number => this.x * other.x + (this.y * other.y)
+
 }
